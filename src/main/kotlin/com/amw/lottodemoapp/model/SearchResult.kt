c25bac413462f17b6917f6f976da3662d8d879c4
warren302 <mikolaj.walkow@gmail.com>
2019-09-22 23:07:31 +0200

    new fields in SearchResult

--- src/main/kotlin/com/amw/lottodemoapp/model/SearchResult.kt
+++ src/main/kotlin/com/amw/lottodemoapp/model/SearchResult.kt
@@ -1,5 +1,24 @@
 package com.amw.lottodemoapp.model
 
+import kotlin.collections.LinkedHashMap
+
 data class SearchResult(val pair: Pair<Int, Int>, val draws : MutableSet<Draw>) {
     val size = draws.size
+    val associatedNumbers = createAssociatedNumbers()
+    val associatedPairs = 1
+
+    private fun createAssociatedNumbers(): Map<Int, Int> {
+        var result = LinkedHashMap<Int, Int>()
+        draws.forEach { element ->
+            element.numbers
+                    .filter { it !in setOf(pair.first, pair.second) }
+                    .forEach { number ->
+                if (result.containsKey(number))
+                    result.put(number, result.getValue(number) + 1)
+                else
+                    result.put(number, 1)
+            }
+        }
+        return result.entries.sortedBy { it.value }.associateBy({it.key}, {it.value})
+    }
 }
--- src/main/kotlin/com/amw/lottodemoapp/service/DrawService.kt
+++ src/main/kotlin/com/amw/lottodemoapp/service/DrawService.kt
@@ -3,8 +3,10 @@ package com.amw.lottodemoapp.service
 import com.amw.lottodemoapp.csv.MyCSVReader
 import com.amw.lottodemoapp.model.Draw
 import com.amw.lottodemoapp.model.SearchResult
+import org.apache.commons.lang3.SystemUtils
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.util.logging.Logger
 import javax.annotation.PostConstruct
 import kotlin.collections.ArrayList
 
@@ -14,6 +16,7 @@ class DrawService {
 
     val CSV_FILENAME = "dl.csv"
 
+
     lateinit var drawList : List<Draw>
     lateinit var twoDim : Array<Array<MutableSet<Draw>>>
 
@@ -21,9 +24,7 @@ class DrawService {
     fun process() : String {
         val csvReader = MyCSVReader()
         drawList = csvReader.convertAllDrawToEntities(CSV_FILENAME)
-        generateTwoDim()
-        uploadTwoDim()
-        printTwoDim()
+        prepareTwoDim()
         return "Done"
     }
 
@@ -36,6 +37,16 @@ class DrawService {
     fun findByQuery(args : Array<String>) : List<SearchResult> {
 
         return getRequestedNumberOfResults(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[3]))
+    }
+
+    private fun prepareTwoDim() {
+        println("---------- preparation of numbers two dimensional matrix started-------------------------------------------------------------------------")
+        val start = System.currentTimeMillis()
+        generateTwoDim()
+        uploadTwoDim()
+        printTwoDim()
+        val elapsedTime = System.currentTimeMillis() - start
+        println("---------- preparation of numbers two dimensional matrix finished in ${System.currentTimeMillis() - start} ms ---------------------------")
     }
 
     private fun generateTwoDim() {
@@ -61,11 +72,9 @@ class DrawService {
     private fun getRequestedNumberOfResults(edge : String, counter : Int, associate : Int) : List<SearchResult> {
         var listOfResults = twoDim[associate].mapIndexed { index, mutableSet -> SearchResult(Pair(associate,index), mutableSet)}.sortedBy { it.size }
         return when (edge.toUpperCase()) {
-            "TOP" -> {listOfResults.takeLast(counter)}
-            "BOTTOM" -> {listOfResults.take(counter)}
-            else -> {
-                throw IllegalArgumentException("Parameter $edge not recognised!")
-            }
+            "TOP"       -> listOfResults.takeLast(counter)
+            "BOTTOM"    -> listOfResults.take(counter)
+            else        -> throw IllegalArgumentException("Parameter $edge not recognised!")
         }
     }
 
